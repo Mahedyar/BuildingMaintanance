049b8398a1320b464cd9cc84f50064d3fa05ea2d
Mahed <mahedyar@gmail.com>
2022-07-26 23:35:48 +0430

    Persian DatePicker added

--- building-maintanance/src/App.js
+++ building-maintanance/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import './App.css';
-import SingleBuilding from './components/singleBuilding/SingleBuilding';
+import React, { useState } from "react";
+import "./App.css";
+import SingleBuilding from "./components/singleBuilding/SingleBuilding";
+
 
 
 function App() {
-  
+ 
+ 
   return (
     <>
-    <SingleBuilding/>
+      <SingleBuilding />
+      {/* <DateTimeInput
+        value={state.myDateTime}
+        name={"myDateTime"}
+        onChange={handleChange}
+      />
+      <DateTimeInputSimple
+        value={state.myDateTime}
+        name={"myDateTime"}
+        onChange={handleChange}
+      /> */}
     
     </>
-   
-    
   );
 }
 
